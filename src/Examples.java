208c85efbcfba243f79d46370467b689e18902a6
ElvisLee0725 <elvislee0725@gmail.com>
2021-12-08 23:42:40 -0800

    Add array of strings

--- src/Examples.java
+++ src/Examples.java
@@ -1,8 +1,18 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Examples {
     public static void main(String[] args) {
+        // For regular array, use Stream.of(array name) to turn it into a Stream
+        String [] nameArr =  new String []{"Al", "Jean", "Bob", "Stacy", "Rob", "Elvis", "Stan", "Steve"};
+        Stream.of(nameArr)
+                .map(String::toUpperCase)
+                .filter(name -> name.startsWith("S"))
+                .sorted()
+                .forEach(System.out::println);
+
+        // A Collection has built in .stream() method
         List<String> nameList = Arrays.asList("Al", "Jean", "Bob", "Stacy", "Rob", "Elvis", "Stan", "Steve");
 
         nameList.stream()
